e3c0f854eb62e68e99166990a12769c1e37b247c
Jarosław Waszczuk <jarekwaszczuk@poczta.fm>
2018-08-08 19:49:16 +0200

    klasa: ChargeResult

--- src/main/java/pl/dominisz/dependencyinjection/ChargeResult.java
+++ src/main/java/pl/dominisz/dependencyinjection/ChargeResult.java
@@ -1,25 +1,11 @@
 package pl.dominisz.dependencyinjection;
 
+import lombok.Data;
+
+@Data
 public class ChargeResult {
 
     private boolean successful;
     private String declineMessage;
-
-    boolean isSuccessful(){
-        return successful;
-    }
-
-    public void setSuccessful(boolean successful) {
-        this.successful = successful;
-    }
-
-    String getDeclineMessage(){
-        return this.declineMessage;
-    }
-
-    public void setDeclineMessage(String declineMessage) {
-        this.declineMessage = declineMessage;
-    }
-
 
 }
