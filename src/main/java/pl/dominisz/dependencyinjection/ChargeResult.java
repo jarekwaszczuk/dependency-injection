54db5cee98f46d913639a349dd6f296ba9826c91
Jarosław Waszczuk <jarekwaszczuk@poczta.fm>
2018-08-08 20:28:11 +0200

    klasa: PaypalCreditCardProcessor (symulacja połączenia z PayPal)

--- src/main/java/pl/dominisz/dependencyinjection/ChargeResult.java
+++ src/main/java/pl/dominisz/dependencyinjection/ChargeResult.java
@@ -1,8 +1,10 @@
 package pl.dominisz.dependencyinjection;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ChargeResult {
 
     private boolean successful;
--- src/main/java/pl/dominisz/dependencyinjection/PaypalCreditCardProcessor.java
+++ src/main/java/pl/dominisz/dependencyinjection/PaypalCreditCardProcessor.java
@@ -1,11 +1,33 @@
 package pl.dominisz.dependencyinjection;
 
 import java.math.BigDecimal;
+import java.util.Random;
 
 public class PaypalCreditCardProcessor implements CreditCardProcessor {
 
-    @Override
-    public ChargeResult charge(CreditCard creditCard, BigDecimal order) {
-        return null;
+    private Random random = new Random();
+
+    public PaypalCreditCardProcessor() {
+        System.out.println("PayPalCreditCardProcessor created");
     }
+
+    @Override
+    public ChargeResult charge(CreditCard creditCard, BigDecimal amount) {
+        System.out.println("Establishing connection to Paypal service...");
+        System.out.println("Charging card " + creditCard.getNumber() + ", amount " + amount);
+
+        int number  = random.nextInt(3);
+
+        if(number==0){
+            System.out.println("Card succesfull charged");
+            return new ChargeResult(true, null);
+        }else if(number==1){
+            System.out.println("Charged decline");
+            return new ChargeResult(false, "Insuffient funds");
+        }else{
+            System.out.println("Connection break");
+            throw new UnreachableException("Connection break");
+        }
+    }
+
 }
