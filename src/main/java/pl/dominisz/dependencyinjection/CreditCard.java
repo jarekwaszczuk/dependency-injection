da4024e71c8a58e2e2e54df1d30835940d5a5ef4
Jarosław Waszczuk <jarekwaszczuk@poczta.fm>
2018-08-08 19:42:33 +0200

    klasa: CreditCard

--- src/main/java/pl/dominisz/dependencyinjection/CreditCard.java
+++ src/main/java/pl/dominisz/dependencyinjection/CreditCard.java
@@ -1,5 +1,53 @@
 package pl.dominisz.dependencyinjection;
 
+import java.time.LocalDate;
+
 public class CreditCard {
+
+    private String number;
+    private String firstName;
+    private String lastName;
+    private LocalDate expireDate;
+    private String cvv;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getExpireDate() {
+        return expireDate;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setExpireDate(LocalDate expireDate) {
+        this.expireDate = expireDate;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
 
 }
