e9a49818b6d57620f0c52814132a2e6515fbdcfb
Jarosław Waszczuk <jarekwaszczuk@poczta.fm>
2018-08-08 19:50:30 +0200

    klasa: PizzaOrder

--- src/main/java/pl/dominisz/dependencyinjection/PizzaOrder.java
+++ src/main/java/pl/dominisz/dependencyinjection/PizzaOrder.java
@@ -1,13 +1,12 @@
 package pl.dominisz.dependencyinjection;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 
+@Data
 public class PizzaOrder {
 
     private BigDecimal amount;
-
-    public BigDecimal getAmount(){
-        return this.amount;
-    }
 
 }
